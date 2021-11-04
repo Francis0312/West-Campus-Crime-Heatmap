6aa0ec03490e63c1189778139b67aeac86c6da66
Francis0312 <pacotron2001@gmail.com>
2021-11-04 08:09:17 -0500

    basic function working!

--- src/heatmap_main.py
+++ src/heatmap_main.py
@@ -1,5 +1,7 @@
 # Changelog
-# v0.1 11/4/2021 6am - building fundamental framework
+# v0.5 11/4/2021 8am - basic heatmap function working
+# v0.2 11/4/2021 6am - filling matrix data
+# v0.1 11/4/2021 5am - building fundamental framework
 #
 # *****************************************************************************
 # A program used to draw a heatmap of all the crime in West Campus, 
@@ -13,6 +15,7 @@
 
 # imports
 import map_bounds # used for internal data matrix 
+import cv2 # openCV2 used for image handling
 import pandas as pd # used to read & analyze csv files
 import numpy as np
 from tkinter import * # Main GUI library
@@ -24,14 +27,87 @@ map_img_path = project_path + "/images/map.png" # background map of west campus
 data_path = project_path + "/data/crime_reports01-2020_11-2021.csv" # data from Jan 2020-Nov 2021
 digits_precision = 4
 
+# gui info
+yellow = '#FFFF00'
+orange = '#FF5733'
+red = '#FF0000'
+circle_radius = 18 # px
+alpha = 0.20
+circle_imgs = []
+
 # Initializes GUI and backend
 def main():
+    # init backend data
     lat_long = data_lat_long(data_path)
     empty_heatmap = init_matrix(lat_long)
     heatmap = fill_matrix(empty_heatmap, lat_long)
-    print(heatmap)
-    #root = Tk()
-    #init_gui(root)
+    
+    # init frontend data
+    root = Tk()
+    canvas = Canvas(root, width = 1166, height = 787)  
+    canvas.pack()  
+    img = ImageTk.PhotoImage(Image.open(map_img_path)) 
+    canvas.create_image(0, 0, anchor=NW, image=img) 
+    init_circles(root)
+    draw_on_map(heatmap, map_img_path, root, canvas)
+    root.mainloop() 
+    
+
+
+def draw_on_map(heatmap, map_img_path, root, canvas):
+    # loading image
+    map_image = cv2.imread(map_img_path)
+    map_height_px = map_image.shape[0]
+    map_length_px = map_image.shape[1]
+
+    # conversion factors
+    pixels_per_lat = float(map_height_px) / heatmap.shape[0]
+    pixels_per_long = float(map_length_px) / heatmap.shape[1]
+
+    # loop through all coordinates
+    for lat in range(heatmap.shape[0]):
+        for long in range(heatmap.shape[1]):
+            # create circle at this point
+            intensity = heatmap[lat][long]
+            x1 = int(pixels_per_long * long)
+            y1 = int(pixels_per_lat * lat)   
+            color = ''
+            if 1 <= intensity < 5 :
+                color = yellow
+                canvas.create_image(x1, y1, image=circle_imgs[0], anchor='nw')
+            elif 5 < intensity < 10:
+                color=orange
+                canvas.create_image(x1, y1, image=circle_imgs[1], anchor='nw')
+            elif intensity >= 10:
+                color=red
+                canvas.create_image(x1, y1, image=circle_imgs[2], anchor='nw')
+            
+            # create_circle(x1, y1, x1+circle_radius, y1+circle_radius,
+            #     root, canvas, fill=color, alpha=alpha)
+            
+
+
+# Creates a circle with alpha value
+def create_circle(x1, y1, x2, y2, root, canvas, **kwargs):      
+    canvas.create_oval(x1, y1, x2, y2, outline='#000000', **kwargs)
+            
+
+# Tkinter hack to add opacity values
+def init_circles(root):
+    alpha_val = int(alpha * 255)
+    red_fill = root.winfo_rgb(red) + (alpha_val,)
+    orange_fill = root.winfo_rgb(orange) + (alpha_val,)
+    yellow_fill = root.winfo_rgb(yellow) + (alpha_val,)
+
+    # Change radii offsets here
+    red_cir = Image.new('RGBA', (circle_radius*2, circle_radius*2), red_fill)
+    orange_cir = Image.new('RGBA', (circle_radius+5, circle_radius+5), orange_fill)
+    yellow_cir = Image.new('RGBA', (circle_radius, circle_radius), yellow_fill)
+
+    circle_imgs.append(ImageTk.PhotoImage(yellow_cir))
+    circle_imgs.append(ImageTk.PhotoImage(orange_cir))
+    circle_imgs.append(ImageTk.PhotoImage(red_cir))
+    
 
 # Fills the heatmap with values
 def fill_matrix(empty_heatmap, lat_long):
@@ -42,7 +118,6 @@ def fill_matrix(empty_heatmap, lat_long):
     data_frame = data_frame[["Latitude","Longitude"]].astype(float) 
     max_latitude, max_longitude, min_latitude, min_longitude = lat_long[0], lat_long[1], lat_long[2], lat_long[3]
 
-    i = 0
     # extract min and max latitudes
     for index, row in data_frame.iterrows():
         cur_lat = truncate(row["Latitude"], digits_precision)
@@ -61,7 +136,7 @@ def fill_matrix(empty_heatmap, lat_long):
                 long_ind -= 1
             # increment value and values of surrounding cells!
             empty_heatmap[lat_ind, long_ind] += 1.0
-            
+
     return empty_heatmap
 
 
@@ -121,22 +196,6 @@ def data_lat_long(data_path):
 def truncate(float_param, N):
     integer = int(float_param * (10**N))/(10**N)
     return float(integer)
-
-
-# Initializes the GUI of the system, loads background map image
-def init_gui(root):
-    canvas = Canvas(root, width = 1166, height = 787)  
-    canvas.pack()  
-    img = ImageTk.PhotoImage(Image.open(map_img_path)) 
-    # Map basics: (longitude, latitude) (N/S, W/E)
-    # Top Left Corner of Map: 
-    # 30.298507512984692, -97.7550797158131
-    # 
-    # Bottom Right Corner of Map:
-    # 30.280448469396532, -97.7242274875346 
-    canvas.create_image(0, 0, anchor=NW, image=img) 
-    root.mainloop() 
-    return None
 
 
 # Driver function for scripting
